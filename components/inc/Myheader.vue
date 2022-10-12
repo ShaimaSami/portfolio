66a2bfc56f28ebe334d0396178fb295aec2e2577
Shaima Sami <shaimaldossari@gmail.com>
2022-10-12 11:58:39 +0300

    Adding Hero + About section

--- components/inc/Myheader.vue
+++ components/inc/Myheader.vue
@@ -3,10 +3,17 @@
     
     <nav class="navbar navbar-expand-lg bg-light shadow-sm p-3 mb-5 bg-body rounded">
   <div class="container-fluid">
+    <a class="navbar-brand" href="#"><span>Portfolio</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse " id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto mb-2 mb-lg-0 ">
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#skills">Skills</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Project</a>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,67 @@
 <template>
-    <div>
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-5 col-md-6 mt-5 pt-5 mb-5">
+                <h1> Hi I'm <span>Shaima</span></h1>
+                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta a 
+                    <br>porro rerum deleniti nesciunt magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natus laudantium commodi quo eaque velit tempore.</h6>
+                    <a
+              href="#about"
+              class="mt-2 btn btn-lg text-white"
+              role="button"
+            >
+              Read More
+            </a>
+            </div>
+            <img src="/images/hero.png" class="col-lg-7 col-md-6 mt-5 pt-5 mb-5" alt="">
+
+        </div>
+        <hr>
+
+        <div id="about" class="row">
+            <h1 class="mt-5 text-center">ABOUT ME</h1>
+            <img src="/images/about.png" class="col-lg-6 col-md-6 mt-5 pt-5 mb-5" alt="">
+
+            <div class="col-lg-6 col-md-6 mt-5 pt-5 mb-5 bg-dark text-white">
+               <h1 class="ms-3">Shaima Al-dossari</h1>
+                <p class="mt-3 ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta a 
+                    <br>porro rerum deleniti nesciunt magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natulis minima fuga facere! laudantium commodi quo eaque velit.
+                    <br>porro rerum deleniti nescis minima fuga facert magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natus laufacilis minima fuga facere! commodi quo eaque velit.
+                    <br>porro rerum deleniti nesciunt magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natulis minima fuga facere! laudantium commodi quo eaque velit.
+                    <br>porro rerum deleniti nescis minima fuga facert magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natus laufacilis minima fuga facere! commodi quo eaque velit.
+                    <br>Quaerat explicabo natus laudantium commodi quo eaque velit tempore.
+                    <br>porro rerum deleniti nesciunt magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natulis minima fuga facere! laudantium commodi quo eaque velit.
+                    <br>porro rerum deleniti nescis minima fuga facert magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natus laufacilis minima fuga facere! commodi quo eaque velit.
+                    <br>porro rerum deleniti nesciunt magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natulis minima fuga facere! laudantium commodi quo eaque velit.
+                    <br>porro rerum deleniti nescis minima fuga facert magni, facilis minima fuga facere!
+                    <br>Quaerat explicabo natus laufacilis minima fuga facere! commodi quo eaque velit.</p>
+                    <a
+              href="#"
+              class="mt-2 ms-3 mb-4 btn btn-lg text-white"
+              role="button"
+            >
+              Resume
+            </a>
+            </div>
+
+        </div>
+
+        <hr>
+
     </div>
+
+    
+
 </template>
 
 <script>

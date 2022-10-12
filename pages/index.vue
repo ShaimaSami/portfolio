177763f1db293a70391482ea184ba916ab8390ef
Shaima Sami <shaimaldossari@gmail.com>
2022-10-12 09:24:35 +0300

    Set Up

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <button type="button" class="btn btn-primary">click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+name: `IndexPage`
+}
+</script>

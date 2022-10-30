3821d8b0f81f0d52f2faaa0e3395158173def656
Shaima Sami <shaimaldossari@gmail.com>
2022-10-30 10:39:40 +0300

    adding router

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  router: {
+    base: '/portfolio/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Portfolio",
@@ -89,9 +93,6 @@ export default {
     '~/plugins/i18n.js',                                                  
   ],
 
-  router: {
-    base: '/portfolio/'
-  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
